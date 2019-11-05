9914d950c685bb3a4a303ad68c5b101baf3e4e75
zhm <frontzhm@163.com>
2019-11-05 18:28:28 +0800

    ok init

---
+++ enforced-table/src/components/PrefixPlusText.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+el-table-column(v-bind="colConfig")
+  template(v-slot="{row}")
+    span {{ parseInt(row[colConfig.prop]) > 0 ? '+' + row[colConfig.prop] : row[colConfig.prop] }}
+</template>
+
+<script>
+export default {
+  props: ['colConfig']
+}
+</script>
+
+<style>
+</style>
