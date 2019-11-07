c0927cf496317c2a014f8e306065d7e5ec1a75df
zhm <frontzhm@163.com>
2019-11-07 15:02:54 +0800

    query-form

--- enhanced-table/src/App.vue
+++ enhanced-table/src/App.vue
@@ -9,8 +9,7 @@
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: #222;
 }
 
 #nav {
--- enhanced-table/src/views/Query.vue
+++ enhanced-table/src/views/Query.vue
@@ -12,7 +12,13 @@ section
         el-button.btn(type='primary', @click='clickResetBtn', plain='') 重置
         el-button.btn(type='warning',plain='', @click='clickExportBtn("AjaxExportClassAssistantEvaluateList")') 导出
   //- 表格区域
-
+  enhanced-table(:is-load='isShowTableLoading', :table-data='tableData', :col-configs='colConfigs')
+    //- 跳转链接 这边注意需要在tableKeys那边加slot
+    el-table-column(slot='className', label='班级名称', align='center')
+      a(slot-scope='{ row }',class="link",href="javascript:;" @click="clickClassName(row)") {{row.className}}
+  //- 分页 没有数据的时候不显示
+  .pagination-box(v-if='isShowPage')
+    el-pagination(@current-change='changeTablePage', :current-page.sync='pageIndex', :page-size='pageSize', layout='prev, pager, next, jumper', :total='tableDataSumCount')
 </template>
 <script>
 import EnhancedTable from '@/components/EnhancedTable'
@@ -37,9 +43,27 @@ export default {
       // label右对齐
       labelPosition: 'right'
     }
+    const colConfigs = [
+      { prop: 'index', label: '序号', sortable: false, align: 'center' },
+      { slot: 'projectName', prop: 'projectName', label: '教研组', sortable: false, align: 'center' },
+      { prop: 'teacherCount', label: '教师数量', sortable: false, align: 'center' },
+      { prop: 'lessonCount', label: '总课次', sortable: false, align: 'center' },
+      { prop: 'uploadPercent', label: '上传率', sortable: false, align: 'center' },
+      { prop: 'passPercent', label: '合格率', sortable: false, align: 'center' }
+    ]
+
     return {
+      // 查询表单项的配置
       formConfig,
-      formAttr
+      // 整个表单的统一配置
+      formAttr,
+      // 表格每列的配置
+      colConfigs,
+      // 表格的加载图标
+      isShowTableLoading: false,
+      // 表格数据
+      tableData: []
+
     }
   }
 
